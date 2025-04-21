8697b3b80001ce48388eb1217caf261e54a09810
Anshuman Parte <parteanshu26@gmail.com>
2025-04-21 23:45:35 +0530

    completed single page routes

--- src/context.jsx
+++ src/context.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState , useContext , useEffect } from 'react';
 const AppContext = React.createContext();
 
-export const API_URL = `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}`;
+export const API_URL = `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}`;
 
 
 
